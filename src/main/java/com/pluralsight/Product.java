df8329fc8fcdfd0d0e157c96b8e8385adc9f3291
Dollant <ariawilliams2425@gmail.com>
2026-04-23 13:33:51 -0400

    Added parameterized constructor to initialize all Product fields at object creation.

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -5,4 +5,10 @@ public class Product {
     private int productID;
     private String name;
     private double price;
+
+    public Product(int productID, String name, double price) {
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+    }
 }
